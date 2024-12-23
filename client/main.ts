853eb008c9ef6e457f3e0bee53bd94f5544e49a8
Yuval.D <Yuval.Dikerman@rayzoneg.com>
2024-12-23 15:16:14 +0200

    ws message

--- client/main.ts
+++ client/main.ts
@@ -1,6 +1,29 @@
+const url = import.meta.env.VITE_BASE_SOCKET_URL
 
-const wb = new WebSocket(import.meta.env.VITE_BASE_SOCKET_URL!)
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Hello World</h1>
-`
+console.log("ws", "Connecting to", url, "...");
+const ws = new WebSocket(url);
+
+ws.addEventListener("message", async (event) => {
+  let data = typeof event.data === "string" ? event.data : await event.data.text();
+  const { user = "system", message = "" } = data.startsWith("{")
+      ? JSON.parse(data)
+      : { message: data };
+
+  console.log(
+      user,
+      typeof message === "string" ? message : JSON.stringify(message),
+  );
+
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = message === "string" ? message : JSON.stringify(message)
+
+});
+
+ws.addEventListener('open', () => {
+  console.log('[ws] connected.')
+})
+
+ws.addEventListener('close', () => {
+  console.log('[ws] disconnected.')
+})
+
 
--- server/main.ts
+++ server/main.ts
@@ -1,3 +1,27 @@
-export default function (req, res) {
-  res.end('Hello World');
-}
+import { defineHooks} from 'crossws'
+
+
+export const websocket = {
+  hooks: defineHooks({
+    open(peer) {
+      peer.send({ user: "server", message: `Welcome ${peer}!` });
+      peer.publish("chat", { user: "server", message: `${peer} joined!` });
+      peer.subscribe("chat");
+    },
+    message(peer, message) {
+      if (message.text().includes("ping")) {
+        peer.send({ user: "server", message: "pong" });
+      } else {
+        const msg = {
+          user: peer.toString(),
+          message: message.toString(),
+        };
+        peer.send(msg); // echo
+        peer.publish("chat", msg);
+      }
+    },
+    close(peer) {
+      peer.publish("chat", { user: "server", message: `${peer} left!` });
+    },
+  }),
+};
